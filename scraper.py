2ee0bfab94904101098043a515954b349e652348
Alex Wilson <a.wilson@alumni.warwick.ac.uk>
2017-12-03 17:53:17 +0000

    Name table and forcibly commit transactions.

--- scraper.py
+++ scraper.py
@@ -7,4 +7,5 @@ data = to_entries(content.content)
 
 print("Importing %d rows" % len(data))
 
-sqlite.save(unique_keys=['position'], data=data)
+sqlite.save(data=data, table="chart")
+sqlite.commit_transactions()
